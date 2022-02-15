c9258692c981161f0d8090c1ea4526fbfb12e06c
Luis Santos <luis.santos.taiba@gmail.com>
2022-02-15 18:21:42 -0300

    feat: model connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require('sequelize');
 const databaseConfig = require('../config/database');
 
+const User = require('../models/User');
+const Animal = require('../models/Animal');
+
+const models = [User, Animal];
+
 class Database {
   constructor() {
     this.init();
@@ -8,6 +13,7 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
+    models.map((model) => model.init(this.connection));
   }
 
   async test() {
